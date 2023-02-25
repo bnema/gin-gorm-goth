f64abeea19d097feffec3b30118a5663abe42a16
Brice Nema <brice@briceamen.com>
2023-02-25 21:01:05 +0100

    Added Get, Update and Verfify funcs to AuthServices

--- services/authServices.go
+++ services/authServices.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"go-gorm-gauth/config"
 	"go-gorm-gauth/models"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -33,7 +34,6 @@ func CreateNewUser(authUser goth.User) (*models.User, error) {
 }
 
 func CreateNewAccount(authUser goth.User) (*models.Account, error) {
-
 	// Check the email, retrieve user.id from database and push it to the account as UserID
 	userFromDB, err := GetUserByEmail(authUser.Email)
 	if err != nil {
@@ -126,5 +126,160 @@ func generateJWT(user *models.User) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	// Sign the token with the secret key
-	return token.SignedString([]byte("secret"))
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func verifyJWT(tokenString string) (*jwt.Token, error) {
+	// Parse the JWT token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the alg is what we expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil {
+		fmt.Println("Error parsing JWT token:", err)
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func GetSessionByToken(tokenString string) (*models.Session, error) {
+	// Verify the JWT token
+	_, err := verifyJWT(tokenString)
+	if err != nil {
+		fmt.Println("Error verifying JWT token:", err)
+		return nil, err
+	}
+
+	// Get the session from the database
+	session := &models.Session{}
+	err = config.DB.Where("session_token = ?", tokenString).First(session).Error
+	if err != nil {
+		fmt.Println("Error getting session by token:", err)
+		// If no session is found with the specified token, return an error
+		if err == gorm.ErrRecordNotFound {
+			return nil, fmt.Errorf("Session not found with token %s", tokenString)
+		}
+		return nil, err
+	}
+
+	fmt.Println("Session found:", session.ID)
+	return session, nil
+}
+
+func GetAccountByProviderAccountID(providerAccountID string) (*models.Account, error) {
+	account := &models.Account{}
+	err := config.DB.Where("provider_account_id = ?", providerAccountID).First(account).Error
+	if err != nil {
+		fmt.Println("Error getting account by provider account id:", err)
+		// If no account is found with the specified provider account id, return an error
+		if err == gorm.ErrRecordNotFound {
+			return nil, fmt.Errorf("Account not found with provider account id %s", providerAccountID)
+		}
+		return nil, err
+	}
+
+	fmt.Println("Account found:", account.ID)
+	return account, nil
+}
+
+func UpdateSession(session *models.Session) (*models.Session, error) {
+	err := config.DB.Save(session).Error
+	if err != nil {
+		fmt.Println("Error updating session:", err)
+		return nil, err
+	}
+
+	fmt.Println("Session updated:", session.ID)
+	return session, nil
+}
+
+func UpdateAccount(account *models.Account) (*models.Account, error) {
+	err := config.DB.Save(account).Error
+	if err != nil {
+		fmt.Println("Error updating account:", err)
+		return nil, err
+	}
+
+	fmt.Println("Account updated:", account.ID)
+	return account, nil
+}
+
+func UpdateUser(user *models.User) (*models.User, error) {
+	err := config.DB.Save(user).Error
+	if err != nil {
+		fmt.Println("Error updating user:", err)
+		return nil, err
+	}
+
+	fmt.Println("User updated:", user.ID)
+	return user, nil
+}
+
+func GetAccountByUserID(userID string) (*models.Account, error) {
+	account := &models.Account{}
+	err := config.DB.Where("user_id = ?", userID).First(account).Error
+	if err != nil {
+		fmt.Println("Error getting account by user id:", err)
+		// If no account is found with the specified user id, return an error
+		if err == gorm.ErrRecordNotFound {
+			return nil, fmt.Errorf("Account not found with user id %s", userID)
+		}
+		return nil, err
+	}
+
+	fmt.Println("Account found:", account.ID)
+	return account, nil
+}
+
+func GetAccountByID(accountID string) (*models.Account, error) {
+	account := &models.Account{}
+	err := config.DB.Where("id = ?", accountID).First(account).Error
+	if err != nil {
+		fmt.Println("Error getting account by id:", err)
+		// If no account is found with the specified id, return an error
+		if err == gorm.ErrRecordNotFound {
+			return nil, fmt.Errorf("Account not found with id %s", accountID)
+		}
+		return nil, err
+	}
+
+	fmt.Println("Account found:", account.ID)
+	return account, nil
+}
+
+func CheckIfAccountExists(providerAccountID string) bool {
+	account := &models.Account{}
+	err := config.DB.Where("provider_account_id = ?", providerAccountID).First(account).Error
+	if err != nil {
+		fmt.Println("Error getting account by provider account id:", err)
+		// If no account is found with the specified provider account id, return false
+		if err == gorm.ErrRecordNotFound {
+			return false
+		}
+		return false
+	}
+
+	fmt.Println("Account found:", account.ID)
+	return true
+}
+
+func CheckIfUserExists(email string) bool {
+	user := &models.User{}
+	err := config.DB.Where("email = ?", email).First(user).Error
+	if err != nil {
+		fmt.Println("Error getting user by email:", err)
+		// If no user is found with the specified email, return false
+		if err == gorm.ErrRecordNotFound {
+			return false
+		}
+		return false
+	}
+
+	fmt.Println("User found:", user.ID)
+	return true
 }
